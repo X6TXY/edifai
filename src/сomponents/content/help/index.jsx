e8e331e1f54570871dc040587017dde0a63c204b
Toleu Bahauddin <baha200477@gmail.com>
2023-07-28 02:13:49 +0600

    update adaptive design assitant

--- src/сomponents/content/help/index.jsx
+++ src/сomponents/content/help/index.jsx
@@ -51,81 +51,81 @@ export const Help = () => {
 
   return (
     <div>
-      <div className="h-screen w-screen bg-[#f5f5f5] text-black">
-        <div className=" sm:w-2/3 md:w-1/2 lg:w-4/5 xl:w-3/4 2xl:w-4/5  h-screen absolute right-0 bg-[#f5f5f5] ">
-          <div className="headinginfoielst mt-10 text-[#C7002B] font-bold flex justify-center text-5xl">
-            IELTS Information
-          </div>
-          <div className="flex justify-center mt-20">
-            <div className="bg-white p-5 flex justify-center w-3/4 drop-shadow-md rounded font-medium">
-              Get IELTS info and resources from our helpful bot! Ace the exam
-              with tips and study materials.
-            </div>
-          </div>
-          <div className="helparea flex justify-center items-center">
-            <form onSubmit={handleSubmit}>
-              <input
-                className="input1  drop-shadow-md h-12 p-3 rounded "
-                type="text"
-                value={inputValue}
-                onChange={handleInputChange}
-                placeholder="Enter your question"
-              />
-              <button
-                type="submit"
-                className="buttonsub btn-primary border drop-shadow-md h-12 p-3"
-              >
-                Submit
-              </button>
-            </form>
-          </div>
-          <div className="flex justify-center items-center ">
-            {isLoading ? (
-              <div className="answer bg-white border drop-shadow-md w-3/4">
-                <div role="status" class="space-y-2.5 animate-pulse max-w-lg flex justify-center">
-                  <div class="flex items-center w-full space-x-2">
-                    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-500 w-32"></div>
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-24"></div>
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-full"></div>
-                  </div>
-                  <div class="flex items-center w-full space-x-2 max-w-[480px]">
-                    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-500 w-full"></div>
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-full"></div>
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-24"></div>
-                  </div>
-                  <div class="flex items-center w-full space-x-2 max-w-[400px]">
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-full"></div>
-                    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-500 w-80"></div>
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-full"></div>
-                  </div>
-                  <div class="flex items-center w-full space-x-2 max-w-[480px]">
-                    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-500 w-full"></div>
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-full"></div>
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-24"></div>
-                  </div>
-                  <div class="flex items-center w-full space-x-2 max-w-[440px]">
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-32"></div>
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-24"></div>
-                    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-500 w-full"></div>
-                  </div>
-                  <div class="flex items-center w-full space-x-2 max-w-[360px]">
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-500 w-full"></div>
-                    <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-500 w-80"></div>
-                    <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-full"></div>
-                  </div>
-                  <span class="sr-only">Loading...</span>
-                </div>
-              </div>
-            ) : typedAnswer ? (
-              <div className="answer w-3/4 bg-white border drop-shadow-md">
-                {typedAnswer}
-              </div>
-            ) : null}
+      <div className="assistantworkingspace ">
+        <div className="assistantheading text-[#c7200b] font-bold">
+          IELTS Information
+        </div>
+        <div className="flex justify-center ">
+          <div className="assistanttext text-black p-3 border">
+            Get IELTS info and resources from our helpful bot! Ace the exam with
+            tips and study materials.
           </div>
         </div>
-
-        <Sidebar />
+        <div className="flex justify-center">
+          <form onSubmit={handleSubmit}>
+            <input
+              className="assistantinputarea  drop-shadow-md h-12 p-3 rounded  text-black "
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+              placeholder="Enter your question"
+            />
+            <button
+              type="submit"
+              className="buttonsub btn-primary border drop-shadow-md h-12 p-3"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center">
+          {isLoading ? (
+            <div className=" bg-white border drop-shadow-md w-1/2 flex justify-center p-2 mt-5">
+              <div
+                role="status"
+                class="space-y-2.5 animate-pulse max-w-lg w-full"
+              >
+                <div class="flex items-center w-full space-x-2 ">
+                  <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-500 w-44"></div>
+                  <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-24"></div>
+                  <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-full"></div>
+                </div>
+                <div class="flex items-center w-full space-x-2 max-w-[480px]">
+                  <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-500 w-full"></div>
+                  <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-full"></div>
+                  <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-24"></div>
+                </div>
+                <div class="flex items-center w-full space-x-2 max-w-[400px]">
+                  <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-full"></div>
+                  <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-500 w-80"></div>
+                  <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-full"></div>
+                </div>
+                <div class="flex items-center w-full space-x-2 max-w-[480px]">
+                  <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-500 w-full"></div>
+                  <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-full"></div>
+                  <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-24"></div>
+                </div>
+                <div class="flex items-center w-full space-x-2 max-w-[440px]">
+                  <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-32"></div>
+                  <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-24"></div>
+                  <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-500 w-full"></div>
+                </div>
+                <div class="flex items-center w-full space-x-2 max-w-[360px]">
+                  <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-500 w-full"></div>
+                  <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-500 w-80"></div>
+                  <div class="h-2.5 bg-gray-300 rounded-full dark:bg-gray-400 w-full"></div>
+                </div>
+                <span class="sr-only">Loading...</span>
+              </div>
+            </div>
+          ) : typedAnswer ? (
+            <div className="assitantanser w-1/2 overflow-auto h-96 mt-5  p-3 bg-white border  text-black ">
+              {typedAnswer}
+            </div>
+          ) : null}
+        </div>
       </div>
+      <Sidebar />
     </div>
   );
 };
