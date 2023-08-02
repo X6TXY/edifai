fad2e0711e0d9f8853362b60ba2c75e70e16bf54
Toleu Bahauddin <baha200477@gmail.com>
2023-08-02 16:20:31 +0600

    analytics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import { Analytics } from '@vercel/analytics/react';
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { RouteList } from "./routes";
-
 
 export const App = () => {
   return (
     <BrowserRouter>
       <RouteList/>
+      <Analytics />
     </BrowserRouter>
     
   );
