bf0f9b2357c1eefa31ac7fd501a6f8b528e1c6c6
Toleu Bahauddin <baha200477@gmail.com>
2023-08-03 19:06:43 +0600

    margin-top for footer

--- src/сomponents/landing/index.jsx
+++ src/сomponents/landing/index.jsx
@@ -119,7 +119,7 @@ export const Landing = () => {
             <img src={IeltsIcon} className="firstimg1" alt="IELTS Main Icon" />
           </div>
         </div>
-        <div className="footersection">
+        <div className="footersection xl:mt-1 lg:mt-12 md:mt-24 sm:mt-16 mt-16">
           <footer class="bg-white rounded-lg shadow m-4 ">
             <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
               <span class="text-sm text-black sm:text-center ">
